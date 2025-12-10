ae27727511ac40e0e5367a718aaf2fb6704b3f64
farzeen-ali <farzeenaliarif786@gmail.com>
2025-12-10 11:01:20 +0500

    OOP Basics

---
+++ script.js
@@ -0,0 +1,23 @@
+// OOP => Object Oriented Programming
+// Class -> blueprint / template
+// Objects -> real world entity
+// Properties -> attributes => data store
+// Methods -> behavior, action
+// Constructors => special method
+
+// class Student {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   introduce() {
+//     console.log(`Hello, I'm ${this.name} & my age is: ${this.age}`);
+//   }
+// }
+
+// const s1 = new Student("Farzeen", 23);
+// console.log(s1.name);
+// console.log(s1.age);
+// s1.introduce();
+
+// car class, model year speed, start accelerate stop 
